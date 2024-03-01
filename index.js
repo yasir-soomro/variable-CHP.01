60b5ce0ff2cefb48860c78b67657fdff8f8bcc6d
yasir soomro <soomroyasir81@gmail.com>
2024-03-02 03:27:46 +0500

    variables chp 01

---
+++ index.js
@@ -0,0 +1,26 @@
+/*let message = "khan"
+console .log(message)
+let penColor= "afzal"
+console .log(penColor)
+let myName = ""
+myName = "soomro";
+console .log(myName);
+let student = {
+ persanolMessage : "I am learning javascript from sharda khapra",
+ studentName : "muhammad afzal",
+   age : 20,
+   isFollow_Apnacolloge_chenal : true,
+};
+student["studentName"]= "yasir soomro"
+student["age"]= 23
+   console .log(student);-*/
+   const ballPen ={
+    penColor: "black",
+    name_:"ballpen",
+    reviews: 4,
+    deal:true,
+    price: "$10",
+    offer:"5%off",
+   };
+   console.log(ballPen);
+
